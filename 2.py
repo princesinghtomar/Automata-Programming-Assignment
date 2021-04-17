e1d8f6e000cba7f9ac83a0e7dec715276ced3ad2
princesinghtomar <singhtomarprince470@gmail.com>
2021-04-17 23:33:59 +0530

    Changes something in 2

--- 2.py
+++ 2.py
@@ -43,37 +43,27 @@ if __name__ == '__main__':
     mapp_states = {}
     final_end_states = []
     final_transition_function = []
-        
     for r in range(0,len(states)+1):
         combinations_list = [list(ele) for ele in list(it.combinations(states, r))]
         final_states = final_states + combinations_list
-
     for i in range(0,len(states)): 
         tmp = []
         mapp_states[states[i]] = i
         for j in range(0,len(letters)): tmp.append([])
         transition_matrix.append(tmp)
-
     for i in range(0,len(letters)): mapp_letters[letters[i]] = i
     k = mapp_letters['$']
-
     for i in range(0,len(transition_function)):
         input_letter = mapp_letters[transition_function[i][1]]
         initial_state = mapp_states[transition_function[i][0]]
         transition_matrix[initial_state][input_letter].append(transition_function[i][2])
-
     for i in range(0,len(states)): epsilon.append(transition_matrix[i][k])
-    # print(epsilon)
     vis = list(it.repeat(0,len(states)))
-    # print(vis)
     for i in range(0,len(states)): epsilon[mapp_states[states[i]]] = bfs(vis,mapp_states[states[i]],epsilon)
-    # print(epsilon)
     for i in range(0,len(epsilon)): epsilon[i] = list(dict.fromkeys(epsilon[i]))
-
     for i in range(0,len(states)):
         transition_matrix[mapp_states[states[i]]][k] = transition_matrix[mapp_states[states[i]]][k] + epsilon[mapp_states[states[i]]]
         transition_matrix[mapp_states[states[i]]][k] = list(dict.fromkeys(transition_matrix[mapp_states[states[i]]][k]))
-
     flen = len(final_states)
     flett = len(final_letters)
     for i in range(0,flen):
@@ -85,12 +75,7 @@ if __name__ == '__main__':
                 for ll in transition_matrix[mapp_states[j]][tt]: ans = ans + epsilon[mapp_states[ll]]
             final_transition_function.append([final_states[i],f"{final_letters[t]}",ans])
         ans = list(dict.fromkeys(ans))
-        
-
-    # calculating start states
     for i in range(0,len(start_states)): final_start_state += epsilon[mapp_states[start_states[i]]]
-
-    #calculating final states
     total_fstate = len(final_states)
     total_estate = len(end_states)
     for i in range(0,total_fstate):
@@ -98,13 +83,11 @@ if __name__ == '__main__':
             if end_states[j] in final_states[i]:
                 final_end_states.append(final_states[i])
                 break
-
     DFA = {
         "states":final_states,
         "letters":final_letters,
         "transition_function":final_transition_function,
         "start_states": [final_start_state],
         "final_states": final_end_states
-    }
-                    
+    }            
     printout(DFA)
